b9f2faa4eea73c76e2a038881a088979db6c8b87
Andy732 <97172761+Andy732@users.noreply.github.com>
2025-09-14 15:56:09 +0300

    Create task1.java

---
+++ task1.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args) {
+        String word = "first ";
+        word += " - second ";
+        
+        System.out.println(word);
+    }
+}
